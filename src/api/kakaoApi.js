2f2b3d74c0812da5dab173863a5dd25ab7013b2e
kimnari1073 <nari1073@naver.com>
2024-07-16 16:21:45 +0900

    api 수정에 따른 추가 코드
    사용자 정보 추출

--- src/api/kakaoApi.js
+++ src/api/kakaoApi.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { API_SERVER_HOST } from "./todoApi";
 const rest_api_key =`e28a154188edbc1fa2616aa44518e959` 
 const redirect_uri =`http://localhost:3000/member/kakao`
 const auth_code_path = `https://kauth.kakao.com/oauth/authorize`
@@ -11,17 +12,24 @@ export const getKakaoLoginLink = () => {
 }
 export const getAccessToken = async (authCode) => {
     const header = {
-    headers: {
-    "Content-Type": "application/x-www-form-urlencoded",
-    }
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        }
     }
     const params = {
-    grant_type: "authorization_code",
-    client_id: rest_api_key,
-    redirect_uri: redirect_uri,
-    code:authCode
+        grant_type: "authorization_code",
+        client_id: rest_api_key,
+        redirect_uri: redirect_uri,
+        code:authCode
     }
     const res = await axios.post(access_token_url, params , header)
     const accessToken = res.data.access_token
     return accessToken
-    }
+}
+
+export const getMemberWithAccessToken = async (accessToken) =>{
+
+    const res = await axios.get(`${API_SERVER_HOST}/api/member/kakao?accessToken=${accessToken}`)
+
+    return res.data
+}
--- src/pages/member/KakaoRedirectPage.js
+++ src/pages/member/KakaoRedirectPage.js
@@ -1,12 +1,15 @@
 import { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
-import { getAccessToken } from "../../api/kakaoApi";
+import { getAccessToken, getMemberWithAccessToken } from "../../api/kakaoApi";
 const KakaoRedirectPage = () => {
     const [searchParams] = useSearchParams()
     const authCode = searchParams.get("code")
     useEffect(() => {
-        getAccessToken(authCode).then(data => {
-        console.log(data)
+        getAccessToken(authCode).then(accessToken => {
+            getMemberWithAccessToken(accessToken).then(result=>{
+                console.log("-----------------------")
+                console.log(result)
+            })
         })
     }, [authCode])
     return (
